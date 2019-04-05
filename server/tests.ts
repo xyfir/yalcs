ffbdd1a7e6c7b106b0a0f4e89b5e0252cfddbcff
MrXyfir <mr@xyfir.com>
2019-04-04 19:56:44 -0700

    Add test for verifySlackRequest()

--- server/tests.ts
+++ server/tests.ts
@@ -2,7 +2,9 @@ import { config } from 'dotenv';
 config();
 import 'enve';
 
+import { verifySlackRequest } from 'lib/verify-slack-request';
 import { MessageStore } from 'lib/MessageStore';
+import { createHmac } from 'crypto';
 import { YALCS } from 'types/yalcs';
 import 'jest-extended';
 
@@ -27,4 +29,14 @@ test('MessageStore', async () => {
   MessageStore.save(thread_ts, _message);
   expect(MessageStore.read(thread_ts)).toMatchObject([_message]);
   expect(MessageStore.read(thread_ts)).toMatchObject([]);
+});
+
+test('verifySlackRequest', () => {
+  const date = Math.floor(Date.now() / 1000);
+  const data = { type: 'event_callback' };
+
+  const hmac = createHmac('sha256', process.enve.SLACK_SIGNING_SECRET);
+  hmac.update(`v0:${date}:${JSON.stringify(data)}`);
+
+  verifySlackRequest(`v0=${hmac.digest('hex')}`, date, data);
 });

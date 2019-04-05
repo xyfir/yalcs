8e43fdc6ddad45ff22ccf8af87450109b74a6543
MrXyfir <mr@xyfir.com>
2019-04-04 18:51:12 -0700

    Listen for thread replies

--- server/lib/slack-listener.ts
+++ server/lib/slack-listener.ts
@@ -1,15 +1,32 @@
 import { verifySlackRequest } from 'lib/verify-slack-request';
+import { MessageStore } from 'lib/MessageStore';
 
 export async function slackListener(
-  event: {
+  data: {
     challenge?: string;
-    token: string;
-    type: 'url_verification';
+    event?: {
+      thread_ts: string;
+      subtype: 'message_replied';
+      channel: string;
+      type: 'message';
+      text: string;
+      ts: string;
+    };
+    type: 'url_verification' | 'event_callback';
   },
   signature: string,
   timestamp: number
 ): Promise<any> {
-  verifySlackRequest(signature, timestamp, event);
+  verifySlackRequest(signature, timestamp, data);
 
-  if (event.type == 'url_verification') return event.challenge;
+  if (data.type == 'url_verification') return data.challenge;
+  if (data.type != 'event_callback') return;
+  if (data.event.channel != process.enve.SLACK_CHANNEL) return;
+  if (data.event.type != 'message') return;
+  if (data.event.subtype != 'message_replied') return;
+
+  MessageStore.save(data.event.thread_ts, {
+    text: data.event.text,
+    ts: data.event.ts
+  });
 }

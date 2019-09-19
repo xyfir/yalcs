bc7baacb38430ccd8ecdd234169cff3eff2e24a5
MrXyfir <mr@xyfir.com>
2019-09-18 19:34:20 -0700

    Mock storage

--- server/__tests__/ThreadStore.spec.ts
+++ server/__tests__/ThreadStore.spec.ts
@@ -1,16 +1,21 @@
 import { ThreadStore } from 'lib/ThreadStore';
 import { Yalcs } from 'types/yalcs';
-import * as uuid from 'uuid/v4';
+import storage from 'node-persist';
+import uuid from 'uuid/v4';
 
 test('ThreadStore', async () => {
-  expect.assertions(4);
+  // Mock storage
+  const mockGetItem = ((storage as any).getItem = jest.fn());
+  const mockSetItem = ((storage as any).setItem = jest.fn());
+  mockGetItem.mockResolvedValueOnce(undefined); // no thread
 
+  // Subscribe to and start thread
   const _thread: Yalcs.Thread = {
     thread_ts: '1568858478.1170',
     messages: [{ text: 'test', ts: '1568858478.1171' }],
     key: uuid()
   };
-
+  mockGetItem.mockResolvedValue(_thread);
   const promise = new Promise(resolve =>
     ThreadStore.subscribe(_thread.thread_ts, thread => {
       expect(thread).toMatchObject(_thread);
@@ -19,15 +24,27 @@ test('ThreadStore', async () => {
   );
   await ThreadStore.save(_thread, true);
   await promise;
+  expect(mockSetItem).toHaveBeenCalledWith('thread-1568858478.1170', _thread);
 
+  // Add message
   _thread.messages = _thread.messages.concat(_thread.messages[0]);
   await ThreadStore.save(_thread);
 
+  // // Load thread
   const thread = await ThreadStore.read(_thread.thread_ts, _thread.key);
   expect(thread).toMatchObject(_thread);
-
   await expect(ThreadStore.read(_thread.thread_ts, '')).toReject();
   await expect(
     ThreadStore.read(_thread.thread_ts, process.enve.SLACK_SIGNING_SECRET)
   ).toResolve();
+
+  // Validate storage calls
+  expect(mockGetItem).toHaveBeenCalledTimes(5);
+  expect(mockGetItem).toHaveBeenCalledWith('thread-1568858478.1170');
+  expect(mockSetItem).toHaveBeenCalledTimes(2);
+  expect(mockSetItem).toHaveBeenNthCalledWith(
+    2,
+    'thread-1568858478.1170',
+    _thread
+  );
 });

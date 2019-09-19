290d324a25f12f4c3f657e9141d02a195588777f
MrXyfir <mr@xyfir.com>
2019-09-18 19:47:55 -0700

    Mock storage

--- server/__tests__/get-thread.spec.ts
+++ server/__tests__/get-thread.spec.ts
@@ -1,16 +1,25 @@
 import { ThreadStore } from 'lib/ThreadStore';
 import { getThread } from 'lib/get-thread';
 import { Yalcs } from 'types/yalcs';
+import storage from 'node-persist';
 import uuid from 'uuid/v4';
 
 test('getThread()', async () => {
+  // Mock storage
+  const mockGetItem = ((storage as any).getItem = jest.fn());
+  const mockSetItem = ((storage as any).setItem = jest.fn());
+  mockGetItem.mockResolvedValueOnce(undefined); // no thread
+
+  // Create thread
   const _thread: Yalcs.Thread = {
     thread_ts: '1568858478.1174',
     messages: [{ text: 'test', ts: '1568858478.1175' }],
     key: uuid()
   };
-
   await ThreadStore.save(_thread);
+  mockGetItem.mockResolvedValueOnce(_thread);
+
+  // Get thread
   const thread = await getThread({
     thread_ts: _thread.thread_ts,
     key: _thread.key

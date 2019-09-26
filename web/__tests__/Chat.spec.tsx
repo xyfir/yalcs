21f10e97aa890bad76182872d050369b208d1cb1
MrXyfir <mr@xyfir.com>
2019-09-26 14:06:40 -0700

    Begin testing polling

--- web/__tests__/Chat.spec.tsx
+++ web/__tests__/Chat.spec.tsx
@@ -10,21 +10,29 @@ import {
 } from '@testing-library/react';
 
 test('<Chat>', async () => {
-  // Mock thread
+  // Mock data
   const thread: Yalcs.Thread = {
     thread_ts: '1569526000.000000',
     messages: [
       {
         outgoing: true,
-        text: 'Hello',
+        text: 'I need help!',
         ts: '1569526001.000000'
       }
     ],
     key: '1f8f8ea7'
   };
+  const message: Yalcs.Message = {
+    text: 'What can I do for you?',
+    ts: '1569526002.000000'
+  };
   const threadOpt: Yalcs.GetThreadOptions = {
     thread_ts: thread.thread_ts,
     key: thread.key
+  };
+  const messageOpt: Yalcs.GetMessageOptions = {
+    message_ts: thread.messages![0].ts,
+    ...threadOpt
   };
 
   // Mock storage
@@ -40,12 +48,15 @@ test('<Chat>', async () => {
   const mockPost = jest.fn();
   const mockGet = jest.fn();
   (api as any).api = { post: mockPost, get: mockGet };
-  mockGet.mockResolvedValueOnce({ data: thread });
+  mockGet.mockResolvedValueOnce({ data: thread }); // get thread
+  mockGet.mockReturnValueOnce(
+    new Promise(r => setTimeout(() => r({ data: [message] }), 1000))
+  ); // polling
 
   // Render Chat
   const { getByLabelText, getByText } = render(<Chat />);
 
-  // Validate API calls
+  // Validate initial API call
   expect(mockGet).toHaveBeenCalledTimes(1);
   expect(mockGet.mock.calls[0][0]).toBe('/thread');
   expect(mockGet.mock.calls[0][1]).toMatchObject({ params: threadOpt });
@@ -54,10 +65,12 @@ test('<Chat>', async () => {
   expect(mockGetItem).toHaveBeenCalledTimes(2);
   expect(mockGetItem).toHaveBeenCalledWith('yalcs.thread_ts');
   expect(mockGetItem).toHaveBeenCalledWith('yalcs.key');
+  await wait(() => expect(mockSetItem).toHaveBeenCalledTimes(2));
+  expect(mockSetItem).toHaveBeenCalledWith('yalcs.thread_ts', thread.thread_ts);
+  expect(mockSetItem).toHaveBeenCalledWith('yalcs.key', thread.key);
 
-  // expect(mockSetItem).toHaveBeenCalledTimes(2);
-  // expect(mockSetItem).toHaveBeenCalledWith('yalcs.thread_ts', thread.thread_ts);
-  // expect(mockSetItem).toHaveBeenCalledWith('yalcs.key', thread.key);
-
-  // Test polling / componentDidUpdate
+  // Validate polling
+  await wait(() => expect(mockGet).toHaveBeenCalledTimes(2));
+  expect(mockGet.mock.calls[1][0]).toBe('/messages');
+  expect(mockGet.mock.calls[1][1]).toMatchObject({ params: messageOpt });
 });
